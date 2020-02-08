7f2a61ec18c59ef63630b7f61040e31b82bec121
pablopsdigital <pablopsdigital@gmail.com>
2020-02-08 20:12:31 +0100

    several samples events- mouse and key

--- mi-proyecto/src/components/Box2.vue
+++ mi-proyecto/src/components/Box2.vue
@@ -1,16 +1,30 @@
 <template>
     <div class="card col-md-4">
         <div class="card-body">
-        <h5 class="card-title">Opción 1</h5>
-
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus tristique nibh id tincidunt.  Aenean nisi massa, scelerisque vel lacus a, fermentum lacinia tellus.</p>
-        <button class="btn btn-primary">Opción 1</button>
+            <h5 class="card-title">Opción 2 - {{entradaTexto}}</h5>
+            <form>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus tristique nibh id tincidunt.  Aenean nisi massa, scelerisque vel lacus a, fermentum lacinia tellus.</p>
+            <div class="row">
+                <input v-model="entradaTexto"/>
+            </div>
+            <div class="row">
+                <boton class="btn btn-secondary" v-on:click="entradaTexto=''">Reset</boton>
+            </div>
+            </form>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name:'Box2'
+    name:'Box2',
+    data() {
+        return {
+            entradaTexto:''
+        }
+    },
+    methods: {
+        
+    },
 }
 </script>
--- mi-proyecto/src/components/Box3.vue
+++ mi-proyecto/src/components/Box3.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="card col-md-4">
         <div class="card-body">
-        <h5 class="card-title">Opción 1</h5>
+        <h5 class="card-title">Opción 3  {{estado}}</h5>
 
         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus tristique nibh id tincidunt.  Aenean nisi massa, scelerisque vel lacus a, fermentum lacinia tellus.</p>
         <button class="btn btn-primary">Opción 1</button>
+        <input @keypress="estado='teclado'" @keypress.a="estado='tecla a'">
+        <a  v-on:click="estado='click'" v-on:mouseover="estado='hover'" class="btn btn-success">Opción 3 </a>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name:'Box3'
+    name:'Box3',
+    data() {
+        return {
+            estado:''
+            
+        }
+    },
 }
 </script>
