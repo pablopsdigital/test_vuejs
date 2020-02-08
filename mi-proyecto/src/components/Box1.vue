9466549306a96193962ed31537ad8432f7f9a18e
pablopsdigital <pablopsdigital@gmail.com>
2020-02-08 19:26:14 +0100

    example use arguments in function

--- mi-proyecto/src/components/Box1.vue
+++ mi-proyecto/src/components/Box1.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="card col-md-4 pl-1">
         <div class="card-body">
-        <h5 class="card-title">Opción 1: {{opcion}}</h5>
+        <h5 class="card-title">{{texto}}</h5>
 
         <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus tristique nibh id tincidunt.  Aenean nisi massa, scelerisque vel lacus a, fermentum lacinia tellus.</p>
-        <button class="btn btn-primary" v-on:click='sumar'>Opción 1</button>
+        <button class="btn btn-primary" v-on:click="letraElegida('Opción 1')">Opción 1</button>
+        <button class="btn btn-primary" v-on:click="letraElegida('Opción 2')">Opción 2</button>
         </div>
     </div>
 </template>
@@ -14,15 +15,17 @@ export default {
     name:'Box1',
     data(){
         return{
-            opcion:''
+            opcion:'',
+            texto: 'Titular'
         }
     },
     methods: {
         sumar: function(){
             this.opcion = Math.floor(Math.random()*1000);
-
+        },
+        letraElegida: function(letra){
+            this.texto = 'Pulsa el botón ' + letra
         }
-        
     },
     
 }
