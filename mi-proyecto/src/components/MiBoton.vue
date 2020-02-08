df7ca66e8feb739af34820e81005c2b0a4897054
pablopsdigital <pablopsdigital@gmail.com>
2020-02-08 10:55:28 +0100

    implemented sample else-if

--- mi-proyecto/src/components/MiBoton.vue
+++ mi-proyecto/src/components/MiBoton.vue
@@ -1,12 +1,29 @@
 <template>
 <div class="MiBoton">
+
+    <!--If-->
     <h1>Ejemplo condicional</h1>
-    {{visible}}
+    <p>Valor variables: Booleano: {{visibleBooleano}}</p>
     <div v-if='visibleBooleano'>
       Usuario autenticado <button :style="btnStyles">{{textoBoton}}</button>
     </div>
+    <p>-------------------------------------------------------------------------</p>
+    <p>Cadena de Texto: {{cadenaTexto}}</p>
     <div v-if="cadenaTexto == 'clave'">
         Usuario autenticado <button :style="btnStyles">{{textoBoton}}</button>
+    </div>
+
+    <!--ElseIf-->
+    <h1>Ejemplo condicional ElseIf</h1>
+    <p>Cadena de Texto: {{cadenaTexto}}</p>
+    <div v-if="cadenaTexto == 'clave'">
+        Usuario autenticado <button :style="btnStyles">{{textoBoton}}</button>
+    </div>
+    <div v-else-if="cadenaTexto">
+        <p>Dentro del else-if - El texto no es igual a {{cadenaTexto}}</p>
+    </div>
+    <div v-else>
+        <p>Dentro else - La propiedad no existe</p>
     </div>
 
 </div>
