3180c4b57b93e265cbe81ac201949b8e7258006a
pablopsdigital <pablopsdigital@gmail.com>
2020-02-08 10:59:36 +0100

    implemented sample v-show

--- mi-proyecto/src/components/MiBoton.vue
+++ mi-proyecto/src/components/MiBoton.vue
@@ -26,6 +26,15 @@
         <p>Dentro else - La propiedad no existe</p>
     </div>
 
+    <!--v-show-->
+    <h1>Ejemplo v-show </h1>
+    <p>Sigue en la estructura html, somo modifica la propiedad display:none). Con if no aparece en el html</p>
+    <p>Siguen ocupando memoria con if no usa espacio de memoria</p>
+    <p>Cadena de Texto: {{cadenaTexto}}</p>
+    <div v-show="cadenaTexto == 'clave'">
+        Usuario autenticado <button :style="btnStyles">{{textoBoton}}</button>
+    </div>
+
 </div>
 </template>
 
