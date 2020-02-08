0c7383d8c3ff2bddf424ada563024efcf398d172
pablopsdigital <pablopsdigital@gmail.com>
2020-02-08 11:41:19 +0100

    install Bootstrap

--- mi-proyecto/src/App.vue
+++ mi-proyecto/src/App.vue
@@ -1,36 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Mi nueva aplicación en Vue.js App"/>
-    <MiComponente></MiComponente>
-    <MiBoton textoBoton='Login' color='orange'></MiBoton>
+    <div class="container">
+      <div class="row">
+          <mi-menu></mi-menu>
+      </div>
+      <div class="row">
+        <section>
+          <h1>Lorem ipsum dolor sit amet</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse rhoncus tristique nibh id tincidunt. Aenean nisi massa, scelerisque vel lacus a, fermentum lacinia tellus. Sed lobortis dui libero, ut pulvinar ipsum vestibulum quis. Maecenas sit amet massa ut leo varius vehicula at quis purus. Fusce eu diam sit amet sapien consectetur luctus. Vestibulum tortor ligula, suscipit et tempus et, suscipit vitae arcu. Integer sed dapibus mi. Aliquam iaculis mauris sed velit placerat tincidunt. Sed aliquet dapibus rutrum. Sed lectus nibh, suscipit sit amet viverra sagittis, euismod ac velit.</p>
+
+          <p>Vestibulum pellentesque massa sollicitudin sem euismod mollis. Vestibulum lobortis imperdiet arcu eget mollis. Fusce tincidunt risus non purus feugiat, et tincidunt mauris ullamcorper. Phasellus quis ante in tellus finibus vulputate quis id nisl. Donec quis risus vel ante ullamcorper pulvinar. Vivamus malesuada ac nisi ullamcorper euismod. Suspendisse semper imperdiet lectus, et finibus neque pellentesque a. Vestibulum pharetra metus vitae neque hendrerit, vel dictum lectus pellentesque. Aenean sodales tortor sit amet rutrum hendrerit. Vestibulum at mauris varius, consequat ex id, maximus lorem. In elementum congue velit et pulvinar. Vestibulum id metus ut quam placerat pellentesque. Integer scelerisque dignissim est. Duis sollicitudin dolor sed leo dapibus, non pulvinar massa semper. Nunc in tempor lorem, placerat volutpat nisl.</p>
+
+          <p>Vestibulum ultricies sem sed risus volutpat, ultrices varius dolor vestibulum. Sed porta tempus malesuada. Vivamus ultrices malesuada tortor vel ultricies. Nulla efficitur urna justo, vel hendrerit risus pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi in tincidunt eros. Aliquam mi libero, tincidunt eget pretium eu, semper sed nulla. Pellentesque sit amet pretium quam. Nullam sit amet ultrices nisi. Maecenas et felis nisl. Aliquam pretium eleifend nunc non bibendum. Praesent placerat turpis ut leo dictum venenatis. Fusce in tellus eget quam tincidunt placerat ut at elit.</p>
+        </section>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import MiComponente from './components/MiComponente.vue'
-import MiBoton from './components/MiBoton.vue'
+import MiMenu from './components/MiMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    MiComponente,
-    MiBoton
+    MiMenu
   }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Titillium+Web&display=swap');
 
-#app {
-  font-family: 'Titillium Web', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
