68b5ebc1fe117382d81e652df1a10b5b8a844829
pablopsdigital <pablopsdigital@gmail.com>
2020-02-08 07:13:43 +0100

    create component

--- mi-proyecto/src/App.vue
+++ mi-proyecto/src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Mi nueva aplicación en Vue.js App"/>
+    <MiComponente></MiComponente>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MiComponente from './components/MiComponente.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MiComponente
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Titillium+Web&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Titillium Web', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ mi-proyecto/src/components/MiComponente.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="MiComponente">
+        <h1>{{libro.titulo}}</h1>
+        <h2>{{libro.autor}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'MiComponente',
+        data() {
+            return {
+                libro:{
+                    titulo:'El método Lean Startup',
+                    autor:'Eric Ries'
+                }
+            }
+        },
+    }
+</script>
+
+
+<style>
+
+</style>
